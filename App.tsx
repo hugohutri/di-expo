5060db22c9588bb3392bda2c374cba0e99fa5500
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-10-18 09:51:59 +0300

    feat: translations

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Translated from "./src/Translated";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Translated />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/Translated.tsx
@@ -0,0 +1,39 @@
+import { View, StyleSheet, Text, Button } from "react-native";
+import * as Localization from "expo-localization";
+import { I18n } from "i18n-js";
+
+// Set the key-value pairs for the different languages you want to support.
+const translations = {
+  en: { welcome: "Hello", name: "Charlie" },
+  ["en-FI"]: { welcome: "Morjest", name: "Kaarle" },
+  ja: { welcome: "こんにちは" },
+};
+const i18n = new I18n(translations);
+
+// Set the locale once at the beginning of your app.
+i18n.locale = "en"; //Localization.locale;
+
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.enableFallback = true;
+// To see the fallback mechanism uncomment line below to force app to use Japanese language.
+// i18n.locale = 'ja';
+
+export default function Translated() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        {i18n.t("welcome")} {i18n.t("name")}
+      </Text>
+      <Text>Current locale: {i18n.locale}</Text>
+      <Text>Device locale: {Localization.locale}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#49a",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
